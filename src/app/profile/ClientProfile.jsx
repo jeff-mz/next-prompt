7a5e433a9231b8c5e25a6b5c12ee769be2bb6589
jafar mirzapoor <jafarmirzapoor11@gmail.com>
2025-05-20 15:54:21 +0330

    last commit.

--- src/app/profile/ClientProfile.jsx
+++ src/app/profile/ClientProfile.jsx
@@ -34,7 +34,7 @@ const ClientProfile = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-6">
+    <div className="max-w-7xl min-h-screen mx-auto p-6">
       <h1 className="text-2xl lg:text-3xl xl:text-5xl font-semibold mb-8 dark:text-white">
         Your Prompts
       </h1>
